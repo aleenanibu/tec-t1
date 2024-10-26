52c9c06a13de944a29ef7bb31746568010ef9ec3
aleenanibu <161929761+aleenanibu@users.noreply.github.com>
2024-10-26 07:37:50 +0530

    Add files via upload

---
+++ excuses.js
@@ -0,0 +1,57 @@
+const excuses = {
+    funny: [
+      "I didn't do my homework because my dog ate it.",
+      // ... other funny excuses
+    ],
+    work: [
+      "I was busy saving the world from a villainous plot at work.",
+      // ... other work-related excuses
+    ],
+    sarcastic: [
+      "Oh, you know, I just thought I'd skip today for fun.",
+      // ... other sarcastic excuses
+    ],
+    creative: [
+      "I was busy creating a masterpiece with my finger paints.",
+      "I was brainstorming ideas for my next big novel.",
+    ],
+    dramatic: [
+      "I was engaged in a tragic love story that kept me away.",
+      "I had to leave suddenly to join a dramatic rescue mission.",
+    ],
+    inspiring: [
+      "I took a day to reflect and rejuvenate my spirit for greater success.",
+      "I needed time to meditate on my goals.",
+    ],
+    historical: [
+      "I was reenacting the signing of the Declaration of Independence.",
+      "I traveled back in time to witness the fall of the Berlin Wall.",
+    ],
+    adventure: [
+      "I was on a quest to find the end of the rainbow.",
+      "I was exploring uncharted territory in my backyard.",
+    ],
+    techy: [
+      "My virtual assistant scheduled me for an urgent update.",
+      "I was debugging my latest invention.",
+    ],
+    holiday: [
+      "I was busy preparing for my annual Halloween costume.",
+      "I had to bake cookies for the holiday party.",
+    ],
+    animal: [
+      "My cat refused to let me leave the house this morning.",
+      "I was rescuing a squirrel from a tree.",
+    ],
+    sporty: [
+      "I was practicing my Olympic qualifying routine.",
+      "I got caught in a spontaneous game of soccer.",
+    ],
+    mystical: [
+      "I was busy consulting my crystal ball about the future.",
+      "I was practicing spells to impress my fellow wizards.",
+    ],
+  };
+  
+
+  
